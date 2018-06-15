217827e3f605c3a810454fe4d6c0691899ea4100
Rplus <Rplus.tw@gmail.com>
2018-06-15 19:24:25 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -130,7 +130,7 @@
           <small>回報確認數</small>
         </div>
         <div class="crop">
-          <img src="http://images.weserv.nl/?url=${report.image.replace(/^https?\:\/\//g, '')}&w=100">
+          <img src="http://images.weserv.nl/?url=${report.image.replace(/^https?\:\/\//g, '')}&w=50&filt=greyscale&il&shape=circle">
         </div>
         <br>
         <a href="${googleNavigation}" target="_blank">google 👣</a>'
@@ -165,5 +165,28 @@
       reports.forEach(setMark);
     });
   }
+
+  function generateFilters() {
+    getTasks().then((tasks) => {
+      console.log({tasks});
+      let dom = tasks.reduce((all, task) => {
+        all.input.push(`<input type="checkbox" class="ckbox-filter" id="ckbox_${task}" checked />`);
+        all.label.push(`<label for="ckbox_${task}"><img src="./img/${task}_.png" title="${task}" /></label>`);
+        all.style.push(`
+          #ckbox_${task}:not(:checked) ~ #map img[src$="/${task}_.png"] { display: none; }
+          #ckbox_${task}:not(:checked) ~ .ctrl img[src$="/${task}_.png"] { filter: contrast(0%); }
+        `);
+        return all;
+      }, { input: [], label: [], style: [] });
+
+      let filter = document.querySelector('.filters');
+      let style = document.createElement('style');
+      style.innerHTML = dom.style.join('');
+      filter.innerHTML = dom.label.join('');
+      filter.insertAdjacentElement('afterbegin', style);
+      document.querySelector('#map').insertAdjacentHTML('beforebegin', dom.input.join(''));
+    });
+  }
+  generateFilters();
 
 // })(window, L);
