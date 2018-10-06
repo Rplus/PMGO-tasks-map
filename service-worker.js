5e1caec8f69719f442d5df726a59001d683fef63
Rplus <rplus.tw@gmail.com>
2018-10-06 11:49:14 +0800

    cache lib in sw

--- service-worker.js
+++ service-worker.js
@@ -14,13 +14,17 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2018-10-06';
+const PRECACHE = 'precache-v2018-10-06.2';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
   './', // Alias for index.html
+
+  './lib/leaflet.css',
+  './lib/leaflet.js',
+
   './css/main.css',
   './js/btn-add-report.js',
   './js/btn-locate-me.js',
